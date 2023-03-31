ff0e54510935848f29a12cc68784302310e90d05
diliupg <diliupg@gmail.com>
2023-03-31 23:53:42 +0530

    L223 Raptor hit react

--- Source/Slash/Private/Enemy/Enemy.cpp
+++ Source/Slash/Private/Enemy/Enemy.cpp
@@ -84,6 +84,10 @@ void AEnemy::GetHit_Implementation( const FVector& ImpactPoint, AActor* Hitter )
 	SetWeaponCollisionEnabled( ECollisionEnabled::NoCollision );
 
 	StopAttackMontage( );
+	if ( IsInsideAttackRadius( ) )
+	{
+		if(!IsDead()) StartAttackTimer( );
+	}
 }
 
 void AEnemy::BeginPlay( )
