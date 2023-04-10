b8e6684946a8828ad52cd1c850e4365c2f7ca5d4
diliupg <diliupg@gmail.com>
2023-04-10 23:30:03 +0530

    L226 Soul drift

--- Source/Slash/Private/Characters/SlashCharacter.cpp
+++ Source/Slash/Private/Characters/SlashCharacter.cpp
@@ -225,6 +225,11 @@ void ASlashCharacter::EKeyPressed( )
 	AWeapon* OverlappingWeapon = Cast<AWeapon>( OverlappingItem );
 	if ( OverlappingWeapon )
 	{
+		if ( EquippedWeapon )
+		{
+			EquippedWeapon->Destroy( );
+		}
+
 		EquipWeapon( OverlappingWeapon );
 	}
 	else
--- Source/Slash/Public/Items/Soul.h
+++ Source/Slash/Public/Items/Soul.h
@@ -13,15 +13,23 @@ UCLASS()
 class SLASH_API ASoul : public AItem
 {
 	GENERATED_BODY()
-	
+public:
+
+	virtual void Tick( float DeltaTime ) override;
+
 protected:
 
+	virtual void BeginPlay( ) override;
 	virtual void OnSphereOverlap( UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult );
 private:
 
 	UPROPERTY(EditAnywhere, Category = SoulProperties )
 	int32 Souls;
 
+	double DesiredZ;
+
+	UPROPERTY(EditAnywhere )
+	float DriftRate = -15.f;
 public:
 
 	FORCEINLINE int32 GetSouls( ) const { return Souls; }
